710d117cacbde2e04fc7c18b149355c7880986ee
MarySweetRollStolen <marylostherself.com>
2022-08-18 10:54:24 +0300

    first empty commit

---
+++ csharp_Starter_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_Starter_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
